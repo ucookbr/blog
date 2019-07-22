4ceecc2abf03971e54b154946c3ed595bca086a8
moraesjeremias <moraesjeremias@users.noreply.github.com>
2019-07-22 14:20:05 -0300

    Create footer.php

---
+++ wp-content/themes/hitchcock-child/footer.php
@@ -0,0 +1,15 @@
+</main><!-- #site-content -->
+
+<div class="credits section-inner">
+			
+	<p>&copy; <?php echo date( 'Y' ); ?> <a href="<?php echo esc_url( home_url() ); ?>"><?php bloginfo( 'name' ); ?></a></p>
+	<p class="theme-by"><?php _e( 'Theme by', 'hitchcock' ); ?> <a href="https://ucook.online">uCook</a></p>
+	
+	<div class="clear"></div>
+	
+</div><!-- .credits -->
+
+<?php wp_footer(); ?>
+
+</body>
+</html>
